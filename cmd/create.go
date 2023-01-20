bd7cb7f313da267d4d212a83a06da71a59c0477f
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-01-21 01:21:08 +0530

    Create `create` command

---
+++ cmd/create.go
@@ -0,0 +1,44 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/Shresht7/gh-gitignore/api"
+	"github.com/spf13/cobra"
+)
+
+//	==============
+//	CREATE COMMAND
+//	==============
+
+// createCmd represents the create command
+var createCmd = &cobra.Command{
+	Use:   "create",
+	Short: "Create a gitignore file",
+	Long:  `Create a gitignore file for your project`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		//	Get the gitignore template name
+		name := args[0]
+
+		//	Get the gitignore template
+		gitignore, err := api.GetGitignoreTemplate(name)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		//	Determine destination
+		var dest string = ".gitignore"
+
+		//	Write license file
+		os.WriteFile(dest, []byte(gitignore.Source), 0644)
+
+	},
+}
+
+func init() {
+	//	Add create command
+	rootCmd.AddCommand(createCmd)
+}
