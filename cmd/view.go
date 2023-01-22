601238f259395571e41bbedeaca4cc519d36ca56
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-01-22 16:53:24 +0530

    Validate template name

--- cmd/view.go
+++ cmd/view.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/Shresht7/gh-gitignore/api"
+	"github.com/Shresht7/gh-gitignore/helpers"
 	"github.com/spf13/cobra"
 )
 
@@ -17,22 +18,29 @@ var viewCmd = &cobra.Command{
 	Aliases: []string{"show", "get"},
 	Short:   "View a gitignore file",
 	Long:    `View a gitignore file in the terminal. Use the "list" command to view all available gitignore templates.`,
-	Args:    cobra.ExactArgs(1),
-	Example: `gh gitignore view node`,
+	Example: `gh gitignore view Node`,
 	Run: func(cmd *cobra.Command, args []string) {
 
-		// Get the gitignore template name
-		name := args[0]
+		// Iterate over all provided gitignore template names
+		for _, name := range args {
 
-		//	Get the gitignore template
-		gitignore, err := api.GetGitignoreTemplate(name)
-		if err != nil {
-			fmt.Println(err)
-			return
+			// Check if the provided gitignore template name is valid
+			if !helpers.IsValidTemplate(name) {
+				fmt.Printf("Invalid gitignore template name: %s\n", name)
+				fmt.Println("\nUse the \"list\" command to view all available gitignore templates.")
+				continue
+			}
+
+			//	Get the gitignore template
+			gitignore, err := api.GetGitignoreTemplate(name)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			//	Print the gitignore template
+			fmt.Println(gitignore.Source)
 		}
-
-		//	Print the gitignore template
-		fmt.Println(gitignore.Source)
 
 	},
 }
