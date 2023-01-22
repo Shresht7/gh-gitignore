da6c33417b259f2ccf0b490b087f0957d13de891
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-01-22 16:41:47 +0530

    Update `view` command details

--- cmd/view.go
+++ cmd/view.go
@@ -11,10 +11,12 @@ import (
 //	VIEW COMMAND
 //	============
 
+// viewCmd represents the view command
 var viewCmd = &cobra.Command{
 	Use:     "view",
-	Short:   "View the gitignore file",
-	Long:    `View the gitignore file`,
+	Aliases: []string{"show", "get"},
+	Short:   "View a gitignore file",
+	Long:    `View a gitignore file in the terminal. Use the "list" command to view all available gitignore templates.`,
 	Args:    cobra.ExactArgs(1),
 	Example: `gh gitignore view node`,
 	Run: func(cmd *cobra.Command, args []string) {
