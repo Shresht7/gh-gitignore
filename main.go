019b63ba1a7a48b339fbd2b1fb5295f11e86a765
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-04-02 06:57:48 +0530

    go mod tidy

--- main.go
+++ main.go
@@ -4,6 +4,10 @@ import (
 	"github.com/Shresht7/gh-gitignore/cmd"
 )
 
+// main is the entry point of the CLI application.
 func main() {
+	// Execute the root command of the CLI application.
+	// This will parse the command line arguments and execute the appropriate command.
+	// The root command is defined in cmd/root.go
 	cmd.Execute()
 }
