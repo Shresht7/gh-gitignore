ab5efda3309e016a5a0212f3db4857e4358aef02
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-02-12 20:02:22 +0530

    Refactor API functions to simplify code

--- api/GetGitignoreTemplate.go
+++ api/GetGitignoreTemplate.go
@@ -2,17 +2,6 @@ package api
 
 // Fetch the gitignore template from the GitHub API
 func GetGitignoreTemplate(name string) (GitignoreTemplateResponse, error) {
-
-	// gitignore templates endpoint
 	endpoint := "gitignore/templates/" + name
-
-	// Get the gitignore template from the GitHub API
-	response, err := request[GitignoreTemplateResponse](endpoint)
-	if err != nil {
-		return GitignoreTemplateResponse{}, err
-	}
-
-	// Return the gitignore template
-	return response, nil
-
+	return request[GitignoreTemplateResponse](endpoint)
 }
--- api/ListGitignoreTemplates.go
+++ api/ListGitignoreTemplates.go
@@ -2,17 +2,6 @@ package api
 
 // List all the gitignore templates from the GitHub API
 func ListGitignoreTemplates() ([]string, error) {
-
-	// gitignore templates endpoint
 	endpoint := "gitignore/templates"
-
-	// Get the gitignore templates from the GitHub API
-	response, err := request[[]string](endpoint)
-	if err != nil {
-		return response, err
-	}
-
-	// Return the gitignore templates
-	return response, nil
-
+	return request[[]string](endpoint)
 }
